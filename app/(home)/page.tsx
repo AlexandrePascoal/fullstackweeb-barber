142c8af4f7989a9fee5d23c1b796bb46a44dd877
Alexandre <alexandre_medeiros11@hotmail.com>
2024-01-30 13:13:54 -0300

    feat: add initial barbershopItem component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,14 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import BarbershopItem from "./_components/barbershop-item";
+import { db } from "../_lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  // chamar o prisma e recuperar as barbearias do banco de dados
+
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -20,6 +26,14 @@ export default function Home() {
       <div className="px-5 mt-5">
         <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
         <BookingItem />
+      </div>
+      <div className="mt-6">
+        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">Recomendados</h2>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   );
